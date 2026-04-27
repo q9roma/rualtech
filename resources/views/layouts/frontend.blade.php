97572956381cba7e50ea0596d72ce29693cd1e96
q9roma <roma.moscow.home@gmail.com>
2026-04-28 01:52:15 +0300

    Подключение Яндекса

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -300,6 +300,34 @@
                 <p>&copy; {{ date('Y') }} Алтех. Все права защищены.</p>
             </div>
         </div>
+
+        {{-- Yandex.Metrika counter --}}
+        <script>
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments); };
+                m[i].l = 1 * new Date();
+                for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=108760077', 'ym');
+
+            window.dataLayer = window.dataLayer || [];
+            ym(108760077, 'init', {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: 'dataLayer',
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce: true,
+                trackLinks: true,
+            });
+        </script>
+        <noscript>
+            <div>
+                <img src="https://mc.yandex.ru/watch/108760077" class="absolute left-[-9999px]" width="1" height="1" alt="">
+            </div>
+        </noscript>
+        {{-- /Yandex.Metrika counter --}}
     </footer>
     </div>
 
